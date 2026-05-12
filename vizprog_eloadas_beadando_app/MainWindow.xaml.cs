6a682221978d1a0ad7be65de2f580a4aafcddffd
mataldominik <mataldominik@gmail.com>
2026-05-12 10:58:39 +0200

    SQL torles funkcio elkeszitese (megerositessel) + frissites funkcio elkeszitese

--- vizprog_eloadas_beadando_app/MainWindow.xaml.cs
+++ vizprog_eloadas_beadando_app/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
 
 
 namespace vizprog_eloadas_beadando_app
@@ -63,6 +64,51 @@ namespace vizprog_eloadas_beadando_app
         private void TargyakBetoltese()
         {
             myDataGrid.ItemsSource = context.Targyak.ToList();
+        }
+
+        private void Hozzaadas_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Modositas_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Torles_click(object sender, RoutedEventArgs e)
+        {
+            if (myDataGrid.SelectedItem == null)
+            {
+                MessageBox.Show("Nincs kijelölt rekord!");
+                return;
+            }
+
+            MessageBoxResult valasz = MessageBox.Show("Biztosan törölni szeretnéd?", "Törlés megerősítése", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (valasz != MessageBoxResult.Yes) return;
+
+            if (aktivTabla == "diakok")
+            {
+                Diak kijeloltDiak = (Diak)myDataGrid.SelectedItem;
+
+                // nem maradhat "árva" rekord, ezért előbb törölni kell a kapcsolódó jegyeket
+                var kapcsolodoJegyek = context.Jegyek.Where(j => j.diakid == kijeloltDiak.id).ToList();
+                context.Jegyek.RemoveRange(kapcsolodoJegyek);
+                // egyből mentünk, hogy ne legyen hiba futáskor
+                context.SaveChanges();
+
+                // majd csak utána jöhet a diák
+                context.Diakok.Remove(kijeloltDiak);
+                context.SaveChanges();
+                DiakokBetoltese();
+            }
+        }
+
+        private void Frissites_click(object sender, RoutedEventArgs e)
+        {
+            if (aktivTabla == "diakok") DiakokBetoltese();
+            else if (aktivTabla == "jegyek") JegyekBetoltese();
+            else if (aktivTabla == "targyak") TargyakBetoltese();
         }
     }
 }
